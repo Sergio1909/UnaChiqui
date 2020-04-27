a0a813cdc57586f115cd1cde2fe046b181713eca
Royer <r.yangali@pucp.pe>
2020-04-27 18:14:18 -0500

    primer commit de Royer

--- src/main/java/sw2/clase1/ejercicio1/controller/HomeController.java
+++ src/main/java/sw2/clase1/ejercicio1/controller/HomeController.java
@@ -1,6 +1,7 @@
 package sw2.clase1.ejercicio1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,4 +11,12 @@ public class HomeController {
     return "index";
 }
 
+@GetMapping("/unachiqui")
+    public String unachiquita(Model model){
+    String x = "gaaaaaaa";
+    model.addAttribute("msg",x);
+    System.out.println("Hola, esta es una prueba jaja");
+    return"/alumno/formCrear";
+
+}
 }
